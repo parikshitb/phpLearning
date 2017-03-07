ca3ed1b52599910e8972f993b7160d7a768680f9
parikshit <parixit23@gmail.com>
2017-03-07 09:34:35 +0530

    variable

--- LoginSuccess.php
+++ LoginSuccess.php
@@ -1,9 +1,4 @@
 <?php
-echo 'Hello User !';
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+echo $_POST['username'];
 
 
