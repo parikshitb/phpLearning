e34dae6cedac0bdbbb164e40e4a7aa08a9b86d57
parikshit <parixit23@gmail.com>
2017-03-07 07:44:31 +0530

    initial commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <form action="LoginSuccess.php" method="post">
+            <label> Username: </label>
+            <input typ="text" id="username" name="username"/>
+            <br /><br />
+            <label> Password: </label>
+            <input type="password" id="password" name="password"/>
+            <br /><br />
+            <input type="submit" id='submit' name='submit'/>
+        </form>
+    </body>
+</html>
