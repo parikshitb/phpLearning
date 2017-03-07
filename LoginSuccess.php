f27edac49705d469f7e3711d328cb4fc5617ff40
parikshit <parixit23@gmail.com>
2017-03-07 17:52:05 +0530

    session - 1

--- LoginSuccess.php
+++ LoginSuccess.php
@@ -1,4 +1,32 @@
 <?php
-echo $_POST['username'];
 
+session_start();
 
+$error=FALSE;
+if (isset($_POST['username']) && isset($_POST['password']) && isset($_POST['submit'])) 
+{
+    $_SESSION['post']=$_POST;
+    if (empty($_POST['username']))
+    {
+        $_SESSION['error']['usernameMessage'] = 'Username empty.';
+        $error = TRUE;
+    }        
+    if (empty($_POST['password']))
+    {
+        $_SESSION['error']['passwordMessage'] = "Password empty.";
+        $error = TRUE;
+    }    
+    elseif (strlen($_POST['password']) < 5)
+    {
+        $_SESSION['error']['passwordMessage'] = "Password too small";
+        $error = TRUE;
+    }
+    if ($error) {
+        header('Location: index.php');
+    }
+}
+else
+{
+    header('Location: index.php');
+}   
+echo 'Login Successful.';
--- index.php
+++ index.php
@@ -1,5 +1,14 @@
 <?php
-    //session_start();
+    
+    session_start();
+        if (isset($_SESSION['error']['usernameMessage'])) 
+        {
+            echo $_SESSION['error']['usernameMessage'];
+        }
+        elseif (isset($_SESSION['error']['passwordMessage'])) 
+        {
+            echo $_SESSION['error']['passwordMessage'];
+        }
 ?>
 <!DOCTYPE html>
 <html>
@@ -8,14 +17,25 @@
         <title></title>
     </head>
     <body>
-        <form action="LoginSuccess.php" method="post">
+        <form action="LoginSuccess.php" method="post" name="">
             <label> Username: </label>
-            <input typ="text" id="username" name="username"/>
+            <input typ="text" id="username" name="username" value="<?php
+                   if (isset($_SESSION['post']['username'])) {
+                       echo $_SESSION['post']['username'];
+                   }
+        ?>"/>
             <br /><br />
             <label> Password: </label>
             <input type="password" id="password" name="password"/>
             <br /><br />
-            <input type="submit" id='submit' name='submit'/>
+            <input type="submit" value='submit' name='submit'/>
         </form>
     </body>
 </html>
+<?php
+if (isset($_SESSION['error'])) {
+        session_unset($_SESSION['error']);
+}
+if (isset($_SESSION['post'])) {
+        session_unset($_SESSION['post']);
+}
